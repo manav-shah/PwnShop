1c8803f4f869b63363672ae1e5b70ea1f4d32d9c
Manav Shah <shahmanav789@gmail.com>
2019-07-08 23:03:47 -0400

    Stores breach data in a list, stores previous data in a txt file

--- PwnShop.py
+++ PwnShop.py
@@ -1,13 +1,27 @@
-import requests
-account='shahmanav789@gmail.com'
-breachdata=requests.get(f'https://haveibeenpwned.com/api/v2/breachedaccount/{account}')
+import requests,json
+#account='shahmanav789@gmail.com'
+account='whatismyname@gmail.com'
+res=requests.get(f'https://haveibeenpwned.com/api/v2/breachedaccount/{account}')
 
+#Catch any http errors without stopping script
 try:
-    breachdata.raise_for_status()
+    res.raise_for_status()
 except Exception as exc:
     print(f'The following error was encountered: {exc}')
 
-tempfile=open('tempfile.txt','wb')
-for chunk in breachdata.iter_content():
-    tempfile.write(chunk)
-tempfile.close()
+#store breach data in a file for future lookup
+with open('tempfile.txt','wb') as tempfile:
+    tempfile.write(res.content)
+    
+breachdata = res.json()
+num_of_breaches=len(breachdata)
+
+
+
+
+
+
+
+
+
+
