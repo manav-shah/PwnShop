c9035d4abc7b756cf65bb3e66ace03287934371f
Manav Shah <shahmanav789@gmail.com>
2019-07-09 22:04:25 -0400

    Modified except statement to only catch HTTP Errors. Added booleans to store whether account was breached

--- PwnShop.py
+++ PwnShop.py
@@ -1,20 +1,25 @@
-import requests,json
-#account='shahmanav789@gmail.com'
-account='whatismyname@gmail.com'
+import requests
+account='shahmanav789@gmail.com'
+#account='somefakeemail_v2@gmail.com'
 res=requests.get(f'https://haveibeenpwned.com/api/v2/breachedaccount/{account}')
 
 #Catch any http errors without stopping script
 try:
     res.raise_for_status()
-except Exception as exc:
-    print(f'The following error was encountered: {exc}')
+    breachdata = res.json()
+    num_of_breaches=len(breachdata)
+    isbreached = True
+except requests.HTTPError as exception:
+    if res.status_code == 404:
+        message = "You haven't been pwned! There were no detected breaches for the given email address."
+        isbreached = False
+    print(f'The following error was encountered: {exception} \n')
 
 #store breach data in a file for future lookup
 with open('tempfile.txt','wb') as tempfile:
     tempfile.write(res.content)
     
-breachdata = res.json()
-num_of_breaches=len(breachdata)
+
 
 
 
