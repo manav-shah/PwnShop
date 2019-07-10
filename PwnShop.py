3f9f6b7c6abb6ca1bd1fbadb33dd44da7e582d08
Manav Shah <shahmanav789@gmail.com>
2019-07-09 23:04:37 -0400

    Added pwned pic to possibly add to message. Script now uses pandas to output a list of breaches sorted by added date (newest first)

--- PwnShop.py
+++ PwnShop.py
@@ -1,6 +1,8 @@
 import requests
-account='shahmanav789@gmail.com'
+import pandas as pd
+#account='shahmanav789@gmail.com'
 #account='somefakeemail_v2@gmail.com'
+account = 'randomname@gmail.com'
 res=requests.get(f'https://haveibeenpwned.com/api/v2/breachedaccount/{account}')
 
 #Catch any http errors without stopping script
@@ -9,13 +11,25 @@ try:
     breachdata = res.json()
     num_of_breaches=len(breachdata)
     isbreached = True
+    message = f"""Uh-oh! Looks like you've been pwned. Your account was found in {num_of_breaches} breaches.\nThese are the detected breaches:\n"""
+    
 except requests.HTTPError as exception:
+    isbreached = False
     if res.status_code == 404:
-        message = "You haven't been pwned! There were no detected breaches for the given email address."
-        isbreached = False
-    print(f'The following error was encountered: {exception} \n')
+        message = "You haven't been pwned (yet)! There were no detected breaches for the given email address."
+    else: 
+        message = f'Sorry, there was an error. The following error was encountered: {exception} \n'
 
-#store breach data in a file for future lookup
+if isbreached:
+    df = pd.DataFrame(breachdata).sort_values(by='AddedDate',ascending=False)
+    domains = [i for i in df['Domain']]
+    addeddate = [i for i in df['AddedDate']]
+    for domain,date in zip(domains,addeddate):
+        message += f'{domain} added to hipb on {date}\n'
+
+
+print(message)
+    #store breach data in a file for future lookup
 #with open('tempfile.txt','wb') as tempfile:
 #    tempfile.write(res.content)
     
