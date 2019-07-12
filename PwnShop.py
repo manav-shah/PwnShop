0fd3019f813ffeb58f6b9416bcc98976fd4d5c1a
Manav Shah <shahmanav789@gmail.com>
2019-07-11 23:55:05 -0400

    Moved pwncheck inside function. Input = account email, output = message

--- PwnShop.py
+++ PwnShop.py
@@ -2,45 +2,45 @@ import requests
 import pandas as pd
 #account='shahmanav789@gmail.com'
 #account='somefakeemail_v2@gmail.com'
-account = 'randomname@gmail.com'
-res=requests.get(f'https://haveibeenpwned.com/api/v2/breachedaccount/{account}')
 
-#Catch any http errors without stopping script
-try:
-    res.raise_for_status()
-    breachdata = res.json()
-    num_of_breaches=len(breachdata)
-    isbreached = True
-    message = f"""Uh-oh! Looks like you've been pwned. Your account was found in {num_of_breaches} breaches.\nThese are the detected breaches:\n"""
-    
-except requests.HTTPError as exception:
-    isbreached = False
-    if res.status_code == 404:
-        message = "You haven't been pwned (yet)! There were no detected breaches for the given email address."
-    else: 
-        message = f'Sorry, there was an error. The following error was encountered: {exception} \n'
+def pwncheck(account):
+    url = f'https://haveibeenpwned.com/api/v2/breachedaccount/{account}'
+    res=requests.get(url)
 
-if isbreached:
-    df = pd.DataFrame(breachdata).sort_values(by='AddedDate',ascending=False)
-    domains = [i for i in df['Domain']]
-    addeddate = [i for i in df['AddedDate']]
-    for domain,date in zip(domains,addeddate):
-        message += f'{domain} added to hipb on {date}\n'
+    #Catch any http errors
+    try:
+        res.raise_for_status()
+        breachdata = res.json()
+        num_of_breaches=len(breachdata)
+        isbreached = True
+        message = [f"""Uh-oh! Looks like you've been pwned. Your account was found in {num_of_breaches} breaches.\nThese are the detected breaches:\n"""]
 
+    except requests.HTTPError as exception:
 
-print(message)
+        isbreached = False
+        if res.status_code == 404: #Not Found Error
+            message = ["You haven't been pwned (yet)! There were no detected breaches for the given email address."]
+        else:
+            message = [f'Sorry, there was an error. The following error was encountered: {exception} \n']
+
+    if isbreached:
+        # TODO: Implement sorting in pure python, so pandas is not a requirement
+        #Convert breachdata from list of dicts to dataframe, so it is easier to sort by added date.
+        df = pd.DataFrame(breachdata).sort_values(by='AddedDate',ascending=False)
+        domains = [i for i in df['Domain']]
+        addeddate = [i for i in df['AddedDate']]
+        for domain,date in zip(domains,addeddate):
+            message.append(f'{domain} added  on {date}\n')
+        message.append(f'For more details, go here: {url}')
+
+    message = ''.join(message)
+    return message
+
+if __name__ == '__main__':
+    account='shahmanav789@gmail.com'
+    message=pwncheck(account)
+    print(message)
+
     #store breach data in a file for future lookup
-#with open('tempfile.txt','wb') as tempfile:
-#    tempfile.write(res.content)
-    
-
-
-
-
-
-
-
-
-
-
-
+    #with open('tempfile.txt','wb') as tempfile:
+    #tempfile.write(res.content)
